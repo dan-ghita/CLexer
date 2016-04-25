adcc8f4cc142c81414c0ff2083fae79ad0a54bca
dan-ghita <dan_ghita_95@yahoo.com>
2016-04-25 10:53:54 +0300

    Improves error reporting

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Parser:
         self.index = 0
         self.error = ''
         self.line_number = 1
+        self.line_index = 0
         self.initial_state = 'init'
 
     @property
@@ -41,6 +42,7 @@ class Parser:
         while self.valid_index and self.is_whitespace(self.current_symbol):
             if self.current_symbol == '\n':
                 self.line_number += 1
+                self.line_index = self.index
             self.index += 1
 
         return self.valid_index
@@ -53,14 +55,20 @@ class Parser:
         return state in self.final_states
 
     def get_token(self):
+        if not self.has_next:
+            self.error = 'Parser has no more tokens'
+            return
+
         token = ''
         current_state = self.initial_state
         line_count = self.line_number
+        line_index = self.line_index
 
         final_state = None
         final_state_index = None
         final_token = None
         final_line_count = None
+        final_line_index = None
 
         while self.valid_index:
             if self.is_final_state(current_state):
@@ -68,24 +76,30 @@ class Parser:
                 final_state_index = self.index
                 final_token = token
                 final_line_count = line_count
+                final_line_index = line_index
 
             symbol = self.current_symbol
-            if symbol == '\n':
-                line_count += 1
 
             transition = (current_state, symbol)
             if transition not in self.dfa:
                 break
+
+            if symbol == '\n':
+                line_count += 1
+                line_index = self.index
 
             token += symbol
             current_state = self.dfa[transition]
             self.index += 1
 
         if not final_state:
-            self.error = 'Error at line: ' + str(
-                line_count) + ' Character: ' + symbol
+            print(line_index)
+            self.error = 'Error at Line: ' + str(line_count) + ' ' + \
+                         'Row: ' + str(self.index - line_index) + ' ' + \
+                         'Unexpected character: ' + repr(symbol)
         else:
             self.line_number = final_line_count
+            self.line_index = final_line_index
             self.index = final_state_index
             state = 'key_word' if final_token in self.key_words else final_state
             return Token(state, final_token)
@@ -96,7 +110,26 @@ def char_range(c1, c2):
         yield chr(c)
 
 
-anything_else = "\'\"_\\/.*-;,!@#+-()[]{}~`:?^&%$#"
+def build_dfa(dfa, state1, symbol, state2):
+    anything_else = "\'\"\\/.*-;,!@#+-()[]{}~`:?^&%$#"
+    if symbol == 'any_char':
+        for c in char_range('a', 'z'):
+            dfa[(state1, c)] = state2
+        for c in char_range('A', 'Z'):
+            dfa[(state1, c)] = state2
+    elif symbol == 'any_digit':
+        for i in range(0, 10):
+            dfa[(state1, str(i))] = state2
+    elif symbol == 'whitespace':
+        dfa[(state1, ' ')] = state2
+        dfa[(state1, '\t')] = state2
+    elif symbol == 'endl':
+        dfa[(state1, '\n')] = state2
+    elif symbol == 'anything_else':
+        for c in anything_else:
+            dfa[(state1, c)] = state2
+    else:
+        dfa[(state1, symbol)] = state2
 
 
 # {(state1, symbol): state2}
@@ -118,28 +151,7 @@ def read_dfa():
             symbol = transition[1]
             state2 = transition[2]
 
-            if symbol == 'any_char':
-                for c in char_range('a', 'z'):
-                    dfa[(state1, c)] = state2
-                for c in char_range('A', 'Z'):
-                    dfa[(state1, c)] = state2
-            elif symbol == 'any_digit':
-                for i in range(0, 10):
-                    dfa[(state1, str(i))] = state2
-            elif symbol == 'whitespace':
-                dfa[(state1, ' ')] = state2
-                dfa[(state1, '\t')] = state2
-                dfa[(state1, '\n')] = state2
-            elif symbol == 'whitespace_no_endl':
-                dfa[(state1, ' ')] = state2
-                dfa[(state1, '\t')] = state2
-            elif symbol == 'anything_else':
-                for c in anything_else:
-                    dfa[(state1, c)] = state2
-            elif symbol == 'endl':
-                dfa[(state1, '\n')] = state2
-            else:
-                dfa[(state1, symbol)] = state2
+            build_dfa(dfa, state1, symbol, state2)
 
         print(dfa)
 
@@ -166,8 +178,5 @@ def main():
             break
         else:
             print(token)
-
-    print(parser.line_number)
-
 
 main()
